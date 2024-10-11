03d5ca0c766221c345f024919d046facb54b0ad9
AviBejjam <avinashbejjam@gmail.com>
2024-10-11 15:59:48 +0530

    update vercel1

--- src/tasks/task-1.js
+++ src/tasks/task-1.js
@@ -5,10 +5,10 @@ const fetchCryptoData = require('../services/fetchCryptoData');
 connectDB();
 
 // Function to run every 2 hours
-const runTask_1 = async () => {
+const runtask1 = async () => {
   await fetchCryptoData(); 
-  setTimeout(runJob, 2 * 60 * 60 * 1000); // Schedule the next run for 2 hours later
+  setTimeout(runtask1, 2 * 60 * 60 * 1000); // Schedule the next run for 2 hours later
 };
 
-runTask_1(); // Start the first job
+runtask1(); // Start the first job
 
